0fbc26ba1c4df49867733f8d26760e70be9e1d70
Eric Uberman <euberman@gmail.com>
2020-09-22 18:49:19 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,10 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
@@ -13,5 +13,7 @@ def link
 end
 
 def all_phrases
-  
+  mario
+  toadstool
+  link
 end
